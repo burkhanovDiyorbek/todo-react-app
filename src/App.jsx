1e8c3adb257a6049018319b7393ea1e6cead9841
Diyorbek <you@example.com>
2024-06-08 19:58:55 +0500

    uptaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import TodoItem from "./TodoItem";
-
+import { v4 as uuid } from "uuid";
+import { Reorder } from "framer-motion";
+import { Modal } from "./Modal";
 function App() {
   const [todosArr, setToDo] = useState(
     JSON.parse(localStorage.getItem("todos")) || []
@@ -13,11 +14,19 @@ function App() {
   useEffect(() => {
     localStorage.setItem("isDark", JSON.stringify(isDark));
   }, [isDark]);
+  const [showModal, setShowModal] = useState(false);
 
   document.body.classList = isDark ? "body-dark" : "body";
 
+  if (showModal) {
+    document.body.style = "overflow:hidden;";
+    window.scrollTo(0, 0);
+  } else {
+    document.body.style = "overflow:visible;";
+  }
+
   const sendData = (obj) => {
-    setToDo((prev) => [obj, ...prev]);
+    setToDo((prev) => [...prev, obj]);
   };
 
   const removeData = (id) => {
@@ -33,11 +42,10 @@ function App() {
   };
 
   const formFunc = (e) => {
-    const date = new Date();
     e.preventDefault();
     if (e.target.children[0].value.trim().length) {
       sendData({
-        id: date.getTime(),
+        id: uuid(),
         title: e.target.children[0].value,
         isComplete: false,
       });
@@ -45,109 +53,162 @@ function App() {
     e.target.children[0].value = "";
   };
 
-  const handleSort = (dragPerson, draggedOverPerson) => {
-    const todos = [...todosArr];
-    const temp = todos[dragPerson.current];
-    todos[dragPerson.current] = todos[draggedOverPerson.person];
-    todos[draggedOverPerson.current] = temp;
-    setToDo(todos);
-    console.log(todos, temp, todosArr);
-  };
-
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todosArr));
   }, [todosArr]);
   return (
-    <div className="container">
-      <div className="container-header">
-        <h2>TODO</h2>
-        <img
-          src="../img/icons/dark.svg"
-          alt="dark"
-          id="dark"
-          onClick={() => setIsDark(!isDark)}
-        />
-        <img
-          src="../img/icons/light.svg"
-          alt="light"
-          id="light"
-          onClick={() => setIsDark(!isDark)}
-        />
-      </div>
-      <div className="container-todo-input">
-        <img src="../img/icons/circle.svg" alt="circle" className="circle" />
-        <img
-          src="../img/icons/circle-dark.svg"
-          alt="circle dark"
-          className="circle-dark"
-        />
-        <form onSubmit={formFunc}>
-          <input type="text" placeholder="Create a new todo…" />
-        </form>
-      </div>
-      <ul className="container-todos">
-        {todosArr
-          .filter((item) =>
+    <>
+      <div className="container">
+        <div className="container-header">
+          <h2>TODO</h2>
+          <img
+            src="../img/icons/dark.svg"
+            alt="dark"
+            id="dark"
+            onClick={() => setIsDark(!isDark)}
+          />
+          <img
+            src="../img/icons/light.svg"
+            alt="light"
+            id="light"
+            onClick={() => setIsDark(!isDark)}
+          />
+        </div>
+        <div className="container-todo-input">
+          <img src="../img/icons/circle.svg" alt="circle" className="circle" />
+          <img
+            src="../img/icons/circle-dark.svg"
+            alt="circle dark"
+            className="circle-dark"
+          />
+          <form onSubmit={formFunc}>
+            <input type="text" placeholder="Create a new todo…" />
+          </form>
+        </div>
+        <Reorder.Group
+          className="container-todos"
+          values={todosArr}
+          onReorder={setToDo}
+          axis="y"
+          as="ul"
+        >
+          {todosArr.filter((item) =>
             filter == "all"
               ? item
               : filter == "completed"
               ? item.isComplete
               : !item.isComplete
-          )
-          .map((item, index) => {
-            return (
-              <TodoItem
-                key={index}
-                item={item}
-                index={index}
-                removeData={removeData}
-                changeIsComp={changeIsComp}
-                handleSort={handleSort}
-              />
-            );
-          })}
-          {!todosArr.length && <li className="li">nothing to see...</li>}
-      </ul>
-      <div className="settings">
-        <div className="todo-count">
-          {todosArr?.filter((item) => !item.isComplete).length} items left
+          ).length ? (
+            todosArr
+              .filter((item) =>
+                filter == "all"
+                  ? item
+                  : filter == "completed"
+                  ? item.isComplete
+                  : !item.isComplete
+              )
+              .map((item) => {
+                return (
+                  <Reorder.Item
+                    className={item.isComplete ? "li completed" : "li"}
+                    value={item}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    as="li"
+                    key={item.id}
+                  >
+                    <img
+                      src="./img/icons/circle.svg"
+                      alt="circle"
+                      className="circle"
+                    />
+                    <img
+                      src="./img/icons/circle-dark.svg"
+                      alt="circle dark"
+                      className="circle-dark"
+                      onClick={() => changeIsComp(item.id)}
+                    />
+                    <img
+                      src="./img/icons/circle-hover.svg"
+                      alt="circle hover"
+                      className="circle-hover"
+                      onClick={() => changeIsComp(item.id)}
+                    />
+                    <img
+                      src="./img/icons/checked.svg"
+                      alt="checked"
+                      className="checked"
+                      onClick={() => changeIsComp(item.id)}
+                    />
+                    <p onClick={() => setShowModal(true)}>{item.title}</p>
+                    <img
+                      onClick={() => removeData(item.id)}
+                      src="./img/icons/remove.svg"
+                      alt="remove"
+                      className="remove"
+                    />
+                    {showModal && (
+                      <Modal
+                        id={item.id}
+                        value={item.title}
+                        setShowModal={setShowModal}
+                        setToDos={setToDo}
+                      />
+                    )}
+                  </Reorder.Item>
+                );
+              })
+          ) : (
+            <li className="li">nothing to see...</li>
+          )}
+        </Reorder.Group>
+        <div className="settings">
+          <div className="todo-count">
+            {todosArr?.filter((item) => !item.isComplete).length} items left
+          </div>
+          <div className="todo-filter">
+            <span
+              className="todo-filter_active all-filter"
+              onClick={() => setFilter("all")}
+            >
+              All
+            </span>
+            <span
+              className="todo-filter_hover active-filter"
+              onClick={() => setFilter("active")}
+            >
+              Active
+            </span>
+            <span
+              className="completed-filter"
+              onClick={() => setFilter("completed")}
+            >
+              Completed
+            </span>
+          </div>
+          <div
+            className="todo-clear"
+            onClick={() =>
+              setToDo((prev) => prev.filter((item) => !item.isComplete))
+            }
+          >
+            Clear Completed
+          </div>
         </div>
-        <div className="todo-filter">
+        <div className="todo-filter todo-filter--mob">
           <span
-            className="todo-filter_active all-filter"
+            className="todo-filter--mob_active"
             onClick={() => setFilter("all")}
           >
             All
           </span>
-          <span
-            className="todo-filter_hover active-filter"
-            onClick={() => setFilter("active")}
-          >
-            Active
-          </span>
-          <span
-            className="completed-filter"
-            onClick={() => setFilter("completed")}
-          >
-            Completed
-          </span>
+          <span onClick={() => setFilter("active")}>Active</span>
+          <span onClick={() => setFilter("completed")}>Completed</span>
         </div>
-        <div
-          className="todo-clear"
-          onClick={() =>
-            setToDo((prev) => prev.filter((item) => !item.isComplete))
-          }
-        >
-          Clear Completed
-        </div>
+        <p className="container-footer">Drag and drop to reorder list</p>
       </div>
-      <div className="todo-filter todo-filter--mob">
-        <span className="todo-filter--mob_active">All</span>
-        <span>Active</span>
-        <span>Completed</span>
-      </div>
-      <p className="container-footer">Drag and drop to reorder list</p>
-    </div>
+    </>
   );
 }
 export default App;
